37f0ac0923bb25fe07cb1c9400d8dc9ea9ce354e
thunpin <thunpin@gmail.com>
2015-06-04 02:04:41 -0300

    [UPDATE] added unique and list methods in SQLHelper

--- src/main/scala/br/tptfc/sdbc/SQLHelper.scala
+++ src/main/scala/br/tptfc/sdbc/SQLHelper.scala
@@ -31,4 +31,91 @@ object SQLHelper {
 
 		SQL.update(query, fields.toList:_*)
 	}
+
+	/**
+	 * get a unique entity
+	 *
+	 * @param table:String - select table
+	 * @param fields:EntityField - entity fields
+	 * @param args:(String, Any) - query where fields->params
+	 * @type entity:Data
+	 */
+	def unique[T] 
+	(
+		table:String, 
+		fields:Seq[String],
+		whereArgs: Seq[(String, Any)]=Nil
+	)
+	(parse:SDBCResult=>T)
+	(implicit connection:Connection):Option[T] = {
+		val sql = createSelect(table, fields:_*)(whereArgs.toList)
+		SQL.unique(sql, whereArgs.map(value=> value._1 -> value._2):_*) (parse)
+	}
+
+	/**
+	 * get a list of entities
+	 *
+	 * @param table:String - select table
+	 * @param fields:EntityField - entity fields
+	 * @param whereArgs:(EntityField, Any) - query where fields->params
+	 * @param orderFields:Seq[EntityField] - order fields
+	 * @param order:Option[String] - order type: "asc" or "desc"
+	 * 
+	 * @return Seq[Data]
+	 */
+	def list[T]
+	(
+		table:String, 
+		fields:Seq[String],
+		whereArgs: Seq[(String, Any)]=Nil,
+		orderFields:Seq[String]=Nil,
+		orderType:Option[String]=None
+	)
+	(parse:SDBCResult=>T)
+	(implicit connection:Connection):Seq[T] = {
+		val sql = createSelect(table, fields:_*)(whereArgs, orderFields, orderType)
+		SQL.select(sql, whereArgs.map(value=> value._1 -> value._2):_*) (parse)
+	}
+
+	/**
+	 * create select query
+	 * 
+	 * @return String query
+	 */
+	private def createSelect
+	(
+		table:String, 
+		fields:String*
+	)
+	(
+		args:Seq[(String,Any)]=Nil, 
+		orderFields:Seq[String]=Nil, 
+		order:Option[String]=None
+	):String = {
+		
+		var sql = 
+		if (fields.isEmpty) { "select * "
+		} else {
+			"select " + fields.map(value => value).mkString(",")
+		}
+
+		sql = sql + " from " + table
+			
+		if (!args.isEmpty) {
+			sql = sql + " where " +
+				args.map(value => value._1 + 
+				"= {" + value._1 + "}").mkString("(", " and ", ")")
+		}
+
+		if (!orderFields.isEmpty) {
+			sql = sql + " order by " +
+			orderFields.map(value => value).mkString(",")
+			order match {
+				case Some(o:String) => sql = sql + " " + o
+				case None => sql = sql + " asc "
+			}
+		}
+
+		sql
+	}
 }
