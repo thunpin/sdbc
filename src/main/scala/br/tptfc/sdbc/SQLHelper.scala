ba2ccf179e7b907e94e38f35cac2d9e3fb57bccf
thunpin <thunpin@gmail.com>
2015-06-03 23:16:29 -0300

    [DEV] added sql helper class

---
+++ src/main/scala/br/tptfc/sdbc/SQLHelper.scala
@@ -0,0 +1,33 @@
+package br.tptfc.sdbc
+
+import java.sql.Connection
+
+object SQLHelper {
+	/**
+	 * persist the data in data base
+	 * 
+	 * @param  {String} table             [table name]
+	 * @param {Map[String,Any]} fields		[map table fields -> values]
+	 */
+	def insert(table:String, fields:Map[String,Any])(implicit connection:Connection):Option[Long] = {
+		val query = "insert into " + table + " " + entity.data.keySet.mkString("(", ",", ")") +
+		" values " + fields.keySet.map(value => "{" + value + "}").mkString("(", ",", ")")
+
+		SQL.insert(query, fields.toList:_*)
+	}
+
+	/**
+	 * update the data
+	 * 
+	 * @param  {String} table             [table name]
+	 * @param  {Map[String,Any]} fields		[fields table with your new value]
+	 * @param  {Map[String,Any]} where		[where args]
+	 */
+	def update(table:Strnig, fields:Map[String,Any], where:Map[String, Any]) {
+		val query = "update " + table + " set " + 
+			fields.keySet.map(value => value + "={" + value + "}").mkString(",") +
+			" where "	+ where.keySet.map(value => value + "={" + value + "}").mkString(" and ")
+
+		SQL.update(query, fields.toList:_*)
+	}
+}
